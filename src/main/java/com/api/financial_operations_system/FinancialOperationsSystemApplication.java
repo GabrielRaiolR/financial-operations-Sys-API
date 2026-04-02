807fa9f06e0e1e91a2371513667ea8867f170e98
Gabriel Raiol Rodrigues <gabrielraiolr@gmail.com>
2026-04-02 10:57:40 -0300

    Core: Start Project Spring

---
+++ src/main/java/com/api/financial_operations_system/FinancialOperationsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.api.financial_operations_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialOperationsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialOperationsSystemApplication.class, args);
+	}
+
+}
