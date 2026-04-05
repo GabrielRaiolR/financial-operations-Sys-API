c73fe49c237b3af430b9e755efb626fa975d6f1a
Gabriel Raiol Rodrigues <gabrielraiolr@gmail.com>
2026-04-05 12:36:18 -0300

    chore: application config, DTO validation, listener and user repository updates

--- src/main/java/com/api/financial_operations_system/messaging/FinancialOrderDomainEventListener.java
+++ src/main/java/com/api/financial_operations_system/messaging/FinancialOrderDomainEventListener.java
@@ -1,7 +1,6 @@
 package com.api.financial_operations_system.messaging;
 
 import com.api.financial_operations_system.events.FinancialOrderCreatedEvent;
-import com.api.financial_operations_system.service.FinancialOrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
--- src/main/java/com/api/financial_operations_system/repository/UserRepository.java
+++ src/main/java/com/api/financial_operations_system/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.api.financial_operations_system.repository;
 
 import com.api.financial_operations_system.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 
 import java.util.Optional;
 import java.util.UUID;
